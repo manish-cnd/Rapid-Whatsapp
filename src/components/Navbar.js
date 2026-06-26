7b9043c5bccb3bd9ec3fff9f6948b5fc3ff96d15
manish-cnd <manishkumartiwari735@gmail.com>
2026-06-26 21:43:07 +0530

    Fix link to GitHub repository in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
             className="btn btn-outline-light"
             target="_blank"
             rel="noreferrer"
-            href="https://github.com/manish-cnd/rapid_whatsapp"
+            href="https://github.com/manish-cnd/Rapid-Whatsapp"
           >
             Code
           </a>
